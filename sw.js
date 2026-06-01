6f973f0fa4f622971790eae59cbe1fa96c3bc663
sandeep <scblogger01@gmail.com>
2026-06-02 02:13:39 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,5 @@
-self.options = {
-    "domain": "5gvci.com",
-    "zoneId": 11057412
-}
-self.lary = ""
-importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
+self.addEventListener('install', event => {
+    self.skipWaiting();
+});
+
+self.addEventListener('fetch', event => {});
